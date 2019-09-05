cd1c528cc521d74ab36f29f844c6572ff452983b
Cristian Medina <cmedina1996@knights.ucf.edu>
2019-09-05 14:58:48 -0400

    Added Zork VS files
    ..

---
+++ Zork/Program.cs
@@ -0,0 +1,16 @@
+﻿// Name: Cristian Medina
+// Date: 9/5/2019
+// Assignment: First Commit to GitHub
+
+using System;
+
+namespace Zork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Zork!");
+        }
+    }
+}
