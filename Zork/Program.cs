0e6f0357d1db65679de920b65e53acb54f52c1c3
cmedina96 <54957802+cmedina96@users.noreply.github.com>
2019-09-11 14:22:36 -0400

    Zork - 3rd Commit
    3 - Finding Your Direction

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Name: Cristian Medina
 // Date: 9/11/2019
-// Assignment: "2 - Looking Around and Quitting"
+// Assignment: "3 - Finding Your Direction"
 
 using System;
 
@@ -13,22 +13,10 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of white house, with a boarded front door.\nArubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
         }
+
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
 }
