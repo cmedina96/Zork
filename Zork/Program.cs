1d2197724f6c53cabd1d3d52415ee661a5f75d00
cmedina96 <54957802+cmedina96@users.noreply.github.com>
2019-09-22 19:27:03 -0400

    Zork - 7th Commit
    7 - Expanding the Frontier - Review

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,154 +1,110 @@
 ﻿// Name: Cristian Medina
 // Date: 9/22/2019
-// Assignment: "6 - Expanding the Frontier"
+// Assignment: "7 - Expanding the Frontier - Review"
 
 using System;
+using System.Collections.Generic;
 
 namespace Zork
 {
     class Program
     {
-        private static string[,] Rooms =
+        private static string CurrentRoom
         {
-            { "Rocky Trail", "South of House", "Canyon View" },
-            { "Forest", "West of House", "Behind House" },
-            { "Dense Woods", "North of House", "Clearing" }
-        };
-
-        /// Store current room location
-        // private static int CurrentRoom = 1;
-        private static int LocationRow = 1;
-        private static int LocationColumn = 1;
-
-        /*
-        - On every iteration of the game loop, display the location of the player (the name of the room).
-
-        - Move takes in Commands, return bool if move was successful.
-        - Within Move, check if Commands is valid input North, South, East, & West.
-        - Throw exception if invalid Commands
-        - Use a Switch for the possible movements
-        - Use length when getting the total length of Rooms arr, don't hardcode.
-        - If they try to move outside array bounds, display "The way is shut!"
-        - Call Move within the switch statements in Main
-        */
-
-        private static bool Move(Commands command)
-        {
-            switch (command)
+            get
             {
-                case Commands.NORTH:
-                    if (LocationRow > 0)
-                    {
-                        LocationRow--;
-                        // Console.WriteLine("You moved NORTH.");
-                        Console.WriteLine(Rooms[LocationRow, LocationColumn]);
-                        return true;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("The way is shut!");
-                        Console.WriteLine(Rooms[LocationRow, LocationColumn]);
-                        return false;
-                    }
-
-                case Commands.SOUTH:
-                    if (LocationRow < Rooms.GetLength(0) - 1) // Check length of rows
-                    {
-                        LocationRow++;
-                        // Console.WriteLine("You moved SOUTH.");
-                        Console.WriteLine(Rooms[LocationRow, LocationColumn]);
-                        return true;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("The way is shut!");
-                        Console.WriteLine(Rooms[LocationRow, LocationColumn]);
-                        return false;
-                    }
-
-                case Commands.WEST:
-                    if (LocationColumn > 0)
-                    {
-                        LocationColumn--;
-                        // Console.WriteLine("You moved WEST.");
-                        Console.WriteLine(Rooms[LocationRow, LocationColumn]);
-                        return true;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("The way is shut!");
-                        Console.WriteLine(Rooms[LocationRow, LocationColumn]);
-                        return false;
-                    }
-
-                case Commands.EAST:
-                    if (LocationColumn < Rooms.GetLength(1) - 1) // Check the length of columns
-                    {
-                        LocationColumn++;
-                        // Console.WriteLine("You moved EAST.");
-                        Console.WriteLine(Rooms[LocationRow, LocationColumn]);
-                        return true;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("The way is shut!");
-                        Console.WriteLine(Rooms[LocationRow, LocationColumn]);
-                        return false;
-                    }
-
-                default:
-                    Console.WriteLine("Invalid commands.");
-                    break;
+                return Rooms[Location.Row, Location.Column];
             }
-
-            return false;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Zork!\n" + Rooms[LocationRow, LocationColumn]);
+            Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
+                Console.WriteLine(CurrentRoom);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                // string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
-                        // outputString = "Thank you for playing!";
                         Console.WriteLine("Thank you for playing!");
                         break;
 
                     case Commands.LOOK:
-                        // outputString = "This is an open field west of a white house, with a boarded front door. A rubber mat saying 'Welcome to Zork!' lies by the door.";
-                        Console.WriteLine("This is an open field west of a white house, with a boarded front door. A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door.");
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.WEST:
                     case Commands.EAST:
-                        Move(command);
-                        // outputString = "You moved " + command;
+                        if (Move(command) == false)
+                        {
+                            Console.WriteLine("The way is shut!");
+                        }
                         break;
 
                     default:
-                        // outputString = "Unknown command.";
                         Console.WriteLine("Unknown command.");
                         break;
                 }
-
-                // Console.WriteLine(outputString);
             }
         }
 
+        private static bool Move(Commands command)
+        {
+            Assert.IsTrue(IsDirection(command), "Invalid direction.");
+
+            bool isValidMove = true;
+            switch (command)
+            {
+                case Commands.NORTH when Location.Row > 0:
+                    Location.Row--;
+                    break;
+
+                case Commands.SOUTH when Location.Row < Rooms.GetLength(0) - 1:
+                    Location.Row++;
+                    break;
+
+                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
+                    Location.Column--;
+                    break;
+
+                case Commands.WEST when Location.Column > 0:
+                    Location.Column++;
+                    break;
+
+                default:
+                    isValidMove = false;
+                    break;
+            }
+
+            return isValidMove;
+        }
+
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+
+        private static bool IsDirection(Commands command) => Directions.Contains(command);
+
+        private static readonly string[,] Rooms = 
+        {
+            { "Rocky Trail", "South of House", "Canyon View" },
+            { "Forest", "West of House", "Behind House" },
+            { "Dense Woods", "North of House", "Clearing" }
+        };
+
+        private static readonly List<Commands> Directions = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
+
+        private static (int Row, int Column) Location = (1, 1);
     }
 }
